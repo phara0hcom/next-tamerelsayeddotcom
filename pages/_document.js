7cedf8211558614f488fde179311f814867f7e7a
Phara0h <t.elsay3d@gmail.com>
2018-08-11 12:45:48 +0900

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,43 @@
 // ./pages/_document.js
 import Document, { Head, Main, NextScript } from 'next/document';
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-46341330-2');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 export default class MyDocument extends Document {
     render() {
         return (
             <html>
                 <Head>
+                    <meta charset="UTF-8" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0"
+                    />
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                    <meta
+                        name="Description"
+                        content="Tamer Elsayed - Web-Designer / Web-Developer, Portfolio"
+                    />
+                    <title>Tamer Elsayed - Web-Designer / Web-Developer</title>
+                    <meta name="theme-color" content="#03072b" />
+
+                    <meta property="og:type" content="WebApp" />
+                    <meta
+                        property="og:site_name"
+                        content="Tamer Elsayed - Web-Designer"
+                    />
+                    <meta property="og:title" content="Tamer Elsayed" />
+                    <meta property="og:url" content="" />
+                    <meta
+                        property="og:description"
+                        content="About Tamer Elsayed, Portfolio"
+                    />
+                    <meta property="og:image" content="" />
+                    <link rel="manifest" href="./static/manifest.json" />
                     <link rel="stylesheet" href="/_next/static/style.css" />
+                    <link
+                        rel="shortcut icon"
+                        type="image/png"
+                        href="./static/favicon.png"
+                    />
                 </Head>
                 <body>
                     <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactGA from 'react-ga';
 
 import Header from '../components/Header/Header';
 import About from '../components/About/About';
@@ -7,6 +8,9 @@ import Portfolio from '../components/Portfolio/Portfolio';
 import Footer from '../components/Footer/Footer';
 
 import '../assets/sass/main.scss';
+
+ReactGA.initialize('UA-46341330-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export default class App extends Component {
     render() {
