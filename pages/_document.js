88a255f5918017a200d2cb63d37682b313a3d3d1
Phara0h <t.elsay3d@gmail.com>
2018-08-11 11:41:44 +0900

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,8 @@
 // ./pages/_document.js
 import Document, { Head, Main, NextScript } from 'next/document';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-46341330-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export default class MyDocument extends Document {
     render() {
