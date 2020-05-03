81aad60da461e49ffef51838914e83a4eb622017
phara0hcom <t.elsay3d@gmail.com>
2020-05-03 13:34:15 +0900

    Prittier

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -3,10 +3,10 @@ import ImageGallery from 'react-image-gallery';
 import { portfolioItems } from './portfolioItems';
 
 const Portfolio = () => {
-    return (
-        <section className="section-portfolio" id="section-portfolio">
-            <ImageGallery items={portfolioItems} />
-        </section>
-    );
+  return (
+    <section className="section-portfolio" id="section-portfolio">
+      <ImageGallery items={portfolioItems} />
+    </section>
+  );
 };
 export default Portfolio;
