ebf72d5bd55079d3e34652dc908951215098ebf5
phara0hcom <t.elsay3d@gmail.com>
2020-05-03 16:52:00 +0900

    meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <title>Tamer Elsayed - Web-Developer</title>
+        <meta
+          name="Description"
+          content="Personal web-site of Tamer Elsayed a Web-Developer, You can find his CV, skills here and links to his Social media accounts. He currently lives in Tokyo, Japan"
+        />
+        <meta
+          name="keywords"
+          content="web-developer,tamer elsayed,elsayed tamer,エルサイエット,テーマァ,software-developer,developer,portfolio,cv,tokyo dev,tokyo web,tokyo developer,tokyo"
+        />
         <Head>
           <link
             rel="preload"
