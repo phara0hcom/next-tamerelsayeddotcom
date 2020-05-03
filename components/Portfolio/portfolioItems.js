9b4bfa222c3633da2a1effbbc66dd9a67f2c823d
phara0hcom <t.elsay3d@gmail.com>
2020-05-03 19:33:41 +0900

    made images smaller

--- components/Portfolio/portfolioItems.js
+++ components/Portfolio/portfolioItems.js
@@ -1,10 +1,10 @@
-import Archer from '../../assets/images/Artcher.me_1900x1200.png';
-import Archer_sml from '../../assets/images/Artcher.me_150x100.png';
+import Archer from '../../assets/images/Artcher.me_1900x1200.jpg';
+import Archer_sml from '../../assets/images/Artcher.me_150x100.jpg';
 import abstract from '../../assets/images/abstract_1900x1200.jpg';
 import abstract_sml from '../../assets/images/abstract_150x100.jpg';
 import Cat from '../../assets/images/CartoonCat_1900x1200.jpg';
 import Cat_sml from '../../assets/images/CartoonCat_150x100.jpg';
-import Light2sound from '../../assets/images/Light2sound_WP_1900x1200.png';
+import Light2sound from '../../assets/images/Light2sound_WP_1900x1200.jpg';
 import Light2sound_sml from '../../assets/images/Light2sound_WP_150x100.png';
 
 export const portfolioItems = [
