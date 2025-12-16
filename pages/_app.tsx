f2e6b6317b1cabda751d7f5725f5bee33d136a74
tamer.elsayed <tamer.elsayed@rakuten.com>
2025-12-16 14:30:26 +0900

    Consent banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
+/**
+ * Note: To get the initial server-side data on other pages, add this to each page:
+ *
+ * import { withInitialC15TData } from '@c15t/nextjs/pages';
+ *
+ * export const getServerSideProps = withInitialC15TData('https://your-instance.c15t.dev');
+ *
+ * This will automatically pass initialC15TData to pageProps.initialC15TData
+ */
+
 import type { AppProps } from 'next/app';
 import '../assets/sass/main.scss';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import { ConsentManager } from "../components/consent-manager";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    		<ConsentManager initialData={pageProps.initialC15TData}>
+    			<Component {...pageProps} />
+    		</ConsentManager>
+    	)
 }
